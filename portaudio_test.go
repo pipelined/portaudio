92aef2c55750522a22854753c879b36f99bada16
dudk <serg.dudk@gmail.com>
2019-08-19 21:19:13 +0200

    devices: disable tests

--- portaudio_test.go
+++ portaudio_test.go
@@ -1,14 +1,12 @@
+// +build integration
+
 package portaudio_test
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"testing"
 
-	"github.com/pipelined/pipe/metric"
-
-	pa "github.com/gordonklaus/portaudio"
 	"github.com/pipelined/pipe"
 	"github.com/pipelined/portaudio"
 	"github.com/pipelined/wav"
@@ -20,59 +18,7 @@ const (
 	wavSample  = "_testdata/sample.wav"
 )
 
-// func TestSink(t *testing.T) {
-// 	sink := portaudio.NewSink(nil)
-
-// 	err := sink.Flush("")
-// 	assert.Nil(t, err)
-
-// 	fn, err := sink.Sink("", 0, 0)
-// 	assert.NotNil(t, fn)
-// 	assert.Nil(t, err)
-
-// 	fn, err = sink.Sink("", 44100, 1)
-// 	assert.NotNil(t, fn)
-// 	assert.Nil(t, err)
-
-// 	err = fn([][]float64{{0, 0, 0}})
-// 	assert.Nil(t, err)
-
-// 	err = sink.Flush("")
-// 	assert.Nil(t, err)
-// }
-
-func TestPADevices(t *testing.T) {
-	pa.Initialize()
-	devices, err := pa.Devices()
-	assert.Nil(t, err)
-	fmt.Printf("%+v\n", devices)
-	for _, d := range devices {
-		fmt.Printf("Device: %v\n", d)
-	}
-
-	// TODO: test if device could be used after init/terminate
-	pa.Initialize()
-	devices, err = pa.Devices()
-	assert.Nil(t, err)
-	fmt.Printf("%+v\n", devices)
-	for _, d := range devices {
-		fmt.Printf("Device: %v\n", d)
-	}
-	pa.Terminate()
-	pa.Terminate()
-}
-
 func TestPipe(t *testing.T) {
-	pa.Initialize()
-	devices, err := portaudio.Devices()
-	assert.Nil(t, err)
-	fmt.Printf("%+v\n", devices)
-	for _, d := range devices {
-		fmt.Printf("Device: %v\n", d)
-	}
-	pa.Terminate()
-	pa.Initialize()
-	pa.Terminate()
 	// create pump
 	inFile, err := os.Open(wavSample)
 	pump := &wav.Pump{ReadSeeker: inFile}
@@ -89,16 +35,10 @@ func TestPipe(t *testing.T) {
 
 	err = pipe.Wait(l.Run(context.Background(), bufferSize))
 	assert.Nil(t, err)
-	fmt.Printf("%+v", metric.GetAll())
-
-	// TODO: validate if init/terminate shows new devices
 }
 
 func TestDevices(t *testing.T) {
 	devices, err := portaudio.Devices()
 	assert.Nil(t, err)
-	fmt.Printf("%+v\n", devices)
-	for _, d := range devices {
-		fmt.Printf("Device: %v\n", d)
-	}
+	assert.NotNil(t, devices)
 }
